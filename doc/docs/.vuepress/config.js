06a159801c2de0244b122b78c12e4405ac68ecb1
ywxgod <ywxgod@gmail.com>
2020-05-04 09:52:49 +0800

    文档示例

---
+++ doc/docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: "element-ui-adapter",
+    description: '根据项目需求，基于element-ui的部分组件进行改造，这里是改造后的组件的使用文档',
+    sidebarDepth: 4,
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'External', link: 'https://google.com' },
+        ],
+        sidebar: [
+            '/',
+            ['/vuepress相关/如何上手', 'Explicit link text']
+        ]
+    }
+};
